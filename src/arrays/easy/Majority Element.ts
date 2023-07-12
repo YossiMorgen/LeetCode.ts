29b6d61193a1614a41d846c1c317ce96eb8c0dbe
yossi <a534158711@gmail.com>
2023-07-12 09:47:46 +0300

    majorityElement O(n)

--- src/arrays/easy/Majority Element.ts
+++ src/arrays/easy/Majority Element.ts
@@ -1,23 +1,44 @@
 function majorityElement(nums: number[]): number {
-    let num: number;
-    let count: number;
-    for(let i = 0; i < nums.length; i++){
-        count = 1
-        if(nums[i]){
-            for(let j = i + 1; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count ++;
-                    delete nums[j];
-                    console.log(count);
-                    
-                }
-            }
-            if(count >= (nums.length / 2)){
-                return nums[i];
-            }
+    nums.sort()
+    let count = 1
+    console.log(nums);
+    for (let index = 0; index < nums.length; index++) {
+        if(nums[index] == nums[index - 1]){
+            count ++
+        } else {
+            count = 1
+        }
+
+        if(count >= (nums.length / 2)){
+            return nums[index];
         }
     }
-
+    
+    return 0
 };
 
 export default majorityElement;
+
+
+
+// function majorityElement(nums: number[]): number {
+    //     let num: number;
+    //     let count: number;
+    //     for(let i = 0; i < nums.length; i++){
+    //         count = 1
+    //         if(nums[i]){
+    //             for(let j = i + 1; j < nums.length; j++){
+    //                 if(nums[i] == nums[j]){
+    //                     count ++;
+    //                     delete nums[j];
+    //                     console.log(count);
+                        
+    //                 }
+    //             }
+    //             if(count >= (nums.length / 2)){
+    //                 return nums[i];
+    //             }
+    //         }
+    //     }
+    
+    // };
