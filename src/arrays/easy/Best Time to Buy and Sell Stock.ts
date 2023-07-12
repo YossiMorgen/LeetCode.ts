bfa2ac249cc47b9aed7140c7584085fe2926ac4b
yossi <a534158711@gmail.com>
2023-07-12 19:55:31 +0300

    maxProfit O(n)

--- src/arrays/easy/Best Time to Buy and Sell Stock.ts
+++ src/arrays/easy/Best Time to Buy and Sell Stock.ts
@@ -1,17 +1,31 @@
 function maxProfit(prices: number[]): number {
     let max = 0;
-
-    for(let i = 0; i < prices.length; i++){
-        for(let j = i; j < prices.length; j ++){
-            if(prices[j] - prices[i] > max){
-                max = prices[j] - prices[i];
-                // console.log(prices[j] + " - " +  prices[i] + " = " + (prices[j] - prices[i]));
-                
-            }
-        }
+    let minNumber = prices[0]
+    for(let i = 1; i < prices.length; i++){
+        minNumber = Math.min(minNumber, prices[i]);
+        max = Math.max(max, prices[i] - minNumber)
     }
-
     return max;
 };
 
 export default maxProfit
+
+
+// function maxProfit(prices: number[]): number {
+//     let max = 0;
+//     for(let i = 0; i < prices.length; i++){
+//         let day2 = i
+//         for(let j = i; j < prices.length; j ++){
+//             if(prices[j] > prices[day2] ){
+//                 day2 = j;
+//             }
+//         }
+//         if(prices[day2] - prices[i] > max){
+//             max = prices[day2] - prices[i]
+//             console.log( prices[day2] + " - " + prices[i] + " = " + (prices[day2] - prices[i]));
+            
+//         }
+//     }
+
+//     return max;
+// };
