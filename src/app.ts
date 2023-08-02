f1b5bdd9b557b43cd8cd7870707db38aa5987de0
yossi <a534158711@gmail.com>
2023-08-02 14:09:27 +0300

    fixed some use cases at zigzag conversion

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,6 @@ import convert from "./strings/medium/Zigzag Conversion";
 
 const date = new Date().getTime()
 
-console.log(convert("PAYPALISHIRING", 4));
-
-
+console.log(convert("PAYPAL", 4));
 
 console.log("time complexity: " + (new Date().getTime() - date));
-
-// "PAYPALISHIRING"
-// "PINALSIGAASRGPI"
-// "PINALSIGAASRGPI"
-// "abcdefghijklmnop"
--- src/strings/medium/Zigzag Conversion.ts
+++ src/strings/medium/Zigzag Conversion.ts
@@ -1,28 +1,22 @@
 export default function convert(str: string, numRows: number): string {
-    if(str.length < numRows || str.length < 2) return str;
+    if(str.length <= numRows || str.length <= 2 || numRows < 2) return str;
     let str2 = '';
-    for(let k = 0; k < numRows; k++){
-        for(let i = 0; i < str.length; i = i + numRows * 2 - 2){
-            console.log(i);
-            if(k == 0){
-                str2 += str.charAt(i)
-                continue;
-            }
+    for(let k = 0; k < numRows; k++){ 
+        let i = 0       
+        while( i < str.length + 2*numRows - 2 ){
 
-            if(k == numRows - 1){
+
+            if( k != numRows - 1 && k != 0 && i-k < str.length ){
                 str2 += str.charAt(i-k)
-                continue;
+                console.log(str.charAt(i-k));
+                
             }
 
-            if(str.charAt(i-k)){
-                str2 += str.charAt(i-k)
+            if(i+k < str.length){
+                str2 += str.charAt(i+k)                
             }
-
-            if(str.charAt(i+1)){
-                str2 += str.charAt(i+1)
-            }
-            // console.log(str2);
             
+            i += 2*numRows - 2;
         }
     }
 
